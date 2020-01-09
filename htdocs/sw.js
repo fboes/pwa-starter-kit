73dc8eec9bdfeaac1d4a387f927701e00265b84f
Frank Boës <boes@web-netz.de>
2020-01-09 09:48:58 +0100

    Updated `sw.js` because https://pwabuilder.com told me to

--- htdocs/sw.js
+++ htdocs/sw.js
@@ -2,35 +2,47 @@
 // @see https://pwabuilder.com
 'use strict';
 
-const cacheName = 'pwa-starter-kit';
-self.addEventListener('install', function(event) {
-  let indexPage = new Request('./');
+const CACHE = "blogophon";
+
+const offlineFallbackPage = "index.html";
+
+self.addEventListener("install", function (event) {
   event.waitUntil(
-    fetch(indexPage).then(function(response) {
-      return caches.open(cacheName).then(function(cache) {
-        return cache.put(indexPage, response);
-      });
+    caches.open(CACHE).then(function (cache) {
+      return cache.add(offlineFallbackPage);
     })
   );
 });
 
-self.addEventListener('fetch', function(event) {
-  let updateCache = function(request){
-    return caches.open(cacheName).then(function(cache) {
-      return fetch(request).then(function(response) {
-        return cache.put(request, response);
-      });
-    });
-  };
-  event.waitUntil(updateCache(event.request));
+self.addEventListener("fetch", function (event) {
+  if (event.request.method !== "GET") return;
+
   event.respondWith(
-    fetch(event.request).catch(function(error) {
-      return caches.open(cacheName).then(function(cache) {
-        return cache.match(event.request).then(function(matching) {
-          let report =  !matching || matching.status === 404 ? Promise.reject('no-match') : matching;
-          return report;
-        });
-      });
-    })
+    fetch(event.request)
+      .then(function (response) {
+        event.waitUntil(updateCache(event.request, response.clone()));
+        return response;
+      })
+      .catch(function (error) {
+        return fromCache(event.request);
+      })
   );
 });
+
+function fromCache(request) {
+  return caches.open(CACHE).then(function (cache) {
+    return cache.match(request).then(function (matching) {
+      if (!matching || matching.status === 404) {
+        return Promise.reject("no-match");
+      }
+
+      return matching;
+    });
+  });
+}
+
+function updateCache(request, response) {
+  return caches.open(CACHE).then(function (cache) {
+    return cache.put(request, response);
+  });
+}
